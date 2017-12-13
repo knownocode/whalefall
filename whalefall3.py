61d6d05ede819f21efdc8b8bf3b5351f100e9ffb
Jay <jaydeslauriers@gmail.com>
2017-12-13 18:14:24 +0000

    update

--- whalefall3.py
+++ whalefall3.py
@@ -1,1 +1,10 @@
 # Python3 new one
+
+import docker
+
+case = docker.from_env()
+
+conts = [cont for cont in case.containers.list(all)]
+
+for idx,cont in enumerate(conts):
+    print("{}) {}".format(idx,val)
