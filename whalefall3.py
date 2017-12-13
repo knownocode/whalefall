e2df3694b6c4b314a43e64f37c7aca617770c54e
Jay <jaydeslauriers@gmail.com>
2017-12-13 18:58:48 +0000

    update

--- whalefall3.py
+++ whalefall3.py
@@ -5,8 +5,22 @@ def main():
     case = docker.from_env()
 
     conts = [cont for cont in case.containers.list(all)]
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for idx,cont in enumerate(conts):
+            print("{}) {} <{}>".format(idx+1, cont.name, cont.short_id))
 
-    for idx,cont in enumerate(conts):
-        print("{}) {}//<{}>".format(idx+1, cont.name, cont.id))
+        choice = input("Detail which container")
+        if choice == 0:
+            break
+        else:
+            ct = conts[choice-1]
+            print("Container Name & <ID>: {name} <{id}>\n \
+                   Image & <Status>: {image} <{status}> \
+                   Processes: {top}".format(name=ct.name,id=ct.short_id,
+                                            image=ct.image,status=ct.status,
+                                            top=ct.top))
+        input("Return to home")
+
 
 main()
